0539ab4bf81f05909e1b5a49b197a8f6be49d69e
d0rc <7993863@gmail.com>
2014-07-01 18:00:30 +0400

    some tests

--- test/nicelb_test.exs
+++ test/nicelb_test.exs
@@ -1,7 +1,18 @@
 defmodule NicelbTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "can read all list" do
+  	list = Nicelb.get_members(:publishers)
+  	assert length(list) == 10000
+  end
+
+  test "can add/remove to different group" do
+  	pid = spawn fn -> :ok end
+  	assert Nicelb.join(:publishersx, pid) == true
+  	assert (Nicelb.get_members(:publishersx) |> length) == 1
+  	assert (Nicelb.get_members(:publishers) |> length) == 10000
+  	assert (Nicelb.get_random_pid(:publishersx) == pid)
+  	Nicelb.leave(:publishersx, pid)
+  	assert (Nicelb.get_members(:publishersx) |> length) == 0
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,2 @@
+Enum.each(1..10000, fn _ -> Nicelb.join(:publishers, spawn(fn -> receive do end end)) end)
 ExUnit.start()
